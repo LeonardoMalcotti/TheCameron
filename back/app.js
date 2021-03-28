bb9080c19f53b668123dd3683ce1a22835c08d1c
LeonardoMalcotti <leonardo.malcotti@studenti.unitn.it>
2021-03-28 15:02:47 +0200

    aggiunta autenticazione come middleware

--- back/app.js
+++ back/app.js
@@ -6,14 +6,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //requires vari
+const tokenChecker = require("./tokenChecker.js");
 
 const users = require("./httpCalls/user.js");
-
+const login = require("./httpCalls/login.js");
 //-------------
 
 //punto d'entrata
 app.use('/',express.static('front/pages'));
 app.use(express.static('front'));
+
+//autenticazione
+app.use("/login",login);
+
+//chiamate http che hanno bisogno di un token
+/*
+app.use("qualcosa",tokenChecker);
+*/
 
 //collegamenti alle chiamate http
 
