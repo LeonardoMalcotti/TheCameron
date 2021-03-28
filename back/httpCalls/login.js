7c9fca1bc275a5c2b47339cd9576f0d86dd61155
LeonardoMalcotti <leonardo.malcotti@studenti.unitn.it>
2021-03-28 14:56:09 +0200

    implementate api per il login

---
+++ back/httpCalls/login.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken'); 
+const user = require('../models/User');
+
+router.post('', async function(req, res) {
+
+	if (!req.body.username){
+		res.status(400).json({ error: "Username dell' utente non specificato" });
+		return;
+	}
+
+	if (!req.body.password){
+		res.status(400).json({ error: "Password dell' utente non specificata" });
+		return;
+	}
+
+	let us = await User.findOne({'username':req.body.username});
+	if(!us){
+		res.status(403).json({ success: false, message: "user not found" });
+		return;
+	}
+
+	if(req.body.password != us.password){
+		res.status(403).json({ success: false, message: "wrong password" });
+		return;
+	}
+
+	var payload = {
+		username: us.username
+		email: us.email
+	};
+
+	var options = {
+		expiresIn: 86400 
+	};
+
+	var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+
+	res.status(200).json({
+		success: true,
+		message: 'Token created',
+		token: token
+	});
+
+});
+
+module.exports = router;
