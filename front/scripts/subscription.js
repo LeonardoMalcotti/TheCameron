c5a21fc0f11c93607acebbcd19e478b22da707c0
elipanni <71708378+Elipanni-Francesca@users.noreply.github.com>
2021-03-29 12:46:15 +0200

    Aggiunte File

--- front/scripts/subscription.js
+++ front/scripts/subscription.js
@@ -15,7 +15,7 @@ function loadSubscription(){
         document.getElementById('abbonamento').innerHTML = "Sei abbonato dal " + data.dateSubscription;
         //paypal non visibile
     })
-    .catch( //error => console.error(error)
+    .catch( error => console.error(error)
 				//document.getElementById('abbonamento').innerHTML = "Non sei abbonato! Abbonati! ";
         //paypal visibile
     );
@@ -25,27 +25,11 @@ function loadSubscription(){
 function addSubscription(){
 		var username = decodeURIComponent(window.location.search).substring(10); //Recupero l'id
 		console.log("username: " + username);
-		var url = '../user/' + username;    //Costruisco l'url con l'id
     var date = new Date().toLocaleDateString("en-IT")
     var user;
-		//document.getElementById('hiddenAbbonamento').value = username;
-    fetch(url, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    })
-    .then((resp) => resp.json())
-    .then(function(data) {
-        console.log(data);
-        user = data;
-        document.getElementById('abb').innerHTML = "Email" + data.email;
-        //paypal non visibile
-    })
-    .catch( //error => console.error(error)
-				//document.getElementById('abb').innerHTML = "Non sei registrato! ";
-        //paypal visibile
-    );
 
-    var url2 = '../user/' + username + '/subscription';
+
+    var url = '../user/' + username + '/subscription';
 
 		fetch(url, {
         method: 'POST',
