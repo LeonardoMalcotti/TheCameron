71247dc254942a0bf556baaeb2924155885097b6
elipanni <71708378+Elipanni-Francesca@users.noreply.github.com>
2021-03-29 16:38:11 +0200

    Correzioni

--- back/httpCalls/user.js
+++ back/httpCalls/user.js
@@ -1,44 +1,42 @@
 const express = require('express');
 const router = express.Router();
 //modello mongoose
-const Users = require('../models/User');
+const User = require('../models/User');
 const Subscription = require('../models/Subscription');
 
 // user/:username GET
 router.get("/:username",async (req,res)=>{
 
-  let user = await User.find({'username':req.params.username}).exec();
+  let user = await User.findOne({'username':req.params.username});
 
 	if(!user){
 		res.status(404).send();
 		return;
 	}
-
-	if(user.stats.length == 0){
+	console.log(user);
+	/*
+	if(user.length == 0){
 		res.status(404).json({errore: "Nessun user trovato"});
 		return;
-	}
+	}*/
 
-	let ret = {
+	res.status(200).json({
 		name : user.name,
-		sruname : user.surname,
+		surname : user.surname,
 		username : user.username,
-		password : user.pass,
+		password : user.password,
 		email : user.email
-	}
+	});
 
-	res.status(200).json(ret);
-
-})
-
+});
 // user/:username/subscription POST
 router.post("/:username/subscription",async(req,res)=>{
 
-	let user = await User.find({'username':req.params.username}).exec();
-  let subscription = await Subscription.find({'username':req.params.username}).exec();
+	let user = await User.findOne({'username':req.params.username});
+  let subscription = await Subscription.findOne({'username':req.params.username});
 
   if(!user){
-		res.status(404).send();
+		res.status(404).json({errore:"Non presente"});
 		return;
 	}
 
@@ -48,14 +46,9 @@ router.post("/:username/subscription",async(req,res)=>{
 	}
 
   if(!subscription){
-		res.status(404).send();
+		res.status(404).json({errore:"Non presente"});
 		return;
-	}
-
-  if (subscription.length != 0){
-		res.status(403).json({ error: "Iscrizione già presente" });
-		return;
-	}
+  }
 
 	let sub = new Subscription({
 		username : req.params.username,
@@ -70,10 +63,10 @@ router.post("/:username/subscription",async(req,res)=>{
 // user/:username/subscription GET
 router.get("/:username/subscription",async (req,res)=>{
 
-  let subscription = await Subscription.find({'username':req.params.username}).exec();
+  let subscription = await Subscription.findOne({'username':req.params.username});
 
 	if(!subscription){
-		res.status(404).send();
+		res.status(404).json({errore:"Non presente"});
 		return;
 	}
 
@@ -82,19 +75,16 @@ router.get("/:username/subscription",async (req,res)=>{
 		return;
 	}
 
-	let ret = {
-		username : subscription.username,
-		dateSubscription : subscription.dateSubscription
-	}
-
-	res.status(200).json(ret);
+	res.status(200).json({username : subscription.username,
+                        dateSubscription : subscription.dateSubscription
+                      });
 
 })
 
 // user/:username/subscription DELETE
 router.delete("/:username/subscription",async (req,res)=>{
 
-  let subscription = await Subscription.find({'username':req.params.username}).exec();
+  let subscription = await Subscription.findOne({'username':req.params.username});
 
 	if(!subscription){
 		res.status(404).send();
