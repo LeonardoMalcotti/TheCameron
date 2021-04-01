f83d5ebdd9d7163af21b04e2ca0a5a717d1fdce6
LeonardoMalcotti <leonardo.malcotti@studenti.unitn.it>
2021-04-01 13:34:13 +0200

    prima implementazione di /filters

---
+++ back/httpCalls/article.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+//modello mongoose
+const Article = require('../models/Article');
+
+/*
+tipi di filtro:
+- genere
+- autore
+- data
+*/
+
+router.get("/filters",async(req,res)=>{
+	let art = await Article.find({});
+
+	function checkFilters(article){
+		var ret = true;
+		if(req.query.author && ret){
+			ret = req.query.autore == article.author;
+		}
+		if(req.query.tags && ret){
+			for( t in req.query.tags ){
+				if(!article.tags.includes(t)){
+					ret = false;
+				}
+			}
+		}
+
+		return ret;
+	}
+
+	function mapFun(article){
+		return {id: article.id, author: article.author};
+	}
+
+	let risp = art.filter(checkFilters).map(mapFun);
+
+	res.status(201).json(risp);
+
+});
