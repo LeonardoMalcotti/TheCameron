957f86098c3e0db756d4f745e6328e896cff8b5e
Filippo Fanton <filippo.fanton@studenti.unitn.it>
2021-04-01 13:56:22 +0200

    Corretto test login

--- back/httpCalls/test/login.test.js
+++ back/httpCalls/test/login.test.js
@@ -4,36 +4,36 @@ const request = supertest(app);
 
 describe('POST /login', () =>{
 
-	//mock function per il metodo find in post /user
-    let findSpy;
+	//mock function per il metodo findOne in post /login
+    let findOneSpy;
 
     beforeAll( () => {
         const User = require("../../models/User");
         /*
-		Mock Implementation per il metodo find in post /user
+		Mock Implementation per il metodo findOne in post /login
 		Finge che nel db esista solo un utente con username "dantealighieri"
 
 		criterias contiene i parametri della chiamata
 		restituisce i dati dell'utente solo se criterias.username è "dantealighieri"
 		in qualsiasi altro caso si comporta come se non avesse trovato nulla
         */
-        findSpy = jest.spyOn(User, 'find').mockImplementation((criterias) =>{
+        findOneSpy = jest.spyOn(User, 'findOne').mockImplementation((criterias) =>{
         	if(criterias.username == "dantealighieri"){
-	            return [{
+	            return {
 	                name: "Dante",  
 	                surname: "Alighieri", 
 	                email: "dante.alighieri@loremipsum.it", 
 	                password: "12345678", 
 	                username: "dantealighieri"
-	            }];
+	            };
 	        } else {
-	        	return [];
+	        	return null;
 	        }
         });
     });
 
     afterAll( async () =>{
-        findSpy.mockRestore();
+        findOneSpy.mockRestore();
     });
 
     //--------------------------------------------------------------------------
@@ -43,6 +43,19 @@ describe('POST /login', () =>{
         const response = await request.post('/login');
 
         expect(response.statusCode).toBe(400);
+
+        done();
+    });
+
+    test('POST /login missing username', async done =>{
+        expect.assertions(1);
+        const response = await request
+            .post('/login')
+            .set('Accept', 'application/json')
+            .send({password: "12345678"});
+            
+        expect(response.statusCode).toBe(400);
+
         done();
     });
 
@@ -51,70 +64,54 @@ describe('POST /login', () =>{
         const response = await request
             .post('/login')
             .set('Accept', 'application/json')
-            .send({username: "dantealighieri",
-                   email: "dante.alighieri@loremipsum.it" 
-            });
+            .send({username: "dantealighieri"});
             
         expect(response.statusCode).toBe(400);
-        done();
-    });
 
-    test('POST /login wrong password', async done =>{
-        expect.assertions(1);
-        const response = await request
-            .post('/login')
-            .set('Accept', 'application/json')
-            .send({email: "dante.alighieri@loremipsum.it", 
-                   password: "87654321" 
-            });
-            
-        expect(response.statusCode).toBe(400);
         done();
     });
 
     test('POST /login valid data', async done =>{
-        expect.assertions(3);
+        //expect.assertions(3);
         const response = await request
             .post('/login')
             .set('Accept', 'application/json')
             .send({username: "dantealighieri",
-                   email: "dante.alighieri@loremipsum.it", 
-                   password: "12345678" 
-            });
-            
+                   password: "12345678"});
+
         expect(response.statusCode).toBe(200);
         expect(response.body.success).toBe(true);
         expect(response.body.token).toBeDefined();
+        
         done();
     });
     
     test('POST /login wrong password', async done =>{
-        expect.assertions(2);
+        //expect.assertions(3);
         const response = await request
             .post('/login')
             .set('Accept', 'application/json')
             .send({username: "dantealighieri",
-                   email: "dante.alighieri@loremipsum.it", 
-                   password: "87654321" 
-            });
+                   password: "87654321"});
             
         expect(response.statusCode).toBe(403);
         expect(response.body.success).toBe(false);
+        expect(response.body.message).toBe("wrong password");
+
         done();
     });
 
     test('POST /login wrong username', async done =>{
-        expect.assertions(2);
+        //expect.assertions(2);
         const response = await request
             .post('/login')
             .set('Accept', 'application/json')
             .send({username: "giovanniboccaccio",
-                   email: "certaldese@loremipsum.it", 
-                   password: "123456789" 
-            });
+                   password: "123456789"});
             
         expect(response.statusCode).toBe(403);
         expect(response.body.success).toBe(false);
+
         done();
     });
     
