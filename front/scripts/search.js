d47d4ce81d323b393f1316bc1feed533fc5d9a0e
Filippo Fanton <filippo.fanton@studenti.unitn.it>
2021-04-02 14:47:08 +0200

    Aggiunto controllo sull'input e fetch in caso di input vuoto

--- front/scripts/search.js
+++ front/scripts/search.js
@@ -1,17 +1,34 @@
 function searchTitle(){
     var text = document.getElementById("txt_search");
+    // Sanificazione dell'input
     text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 
-    fetch('../article/search/'+text, {
-        method: GET,
-        headers:{ 'Content-Type': 'application/json' }
-    })
-    .then((resp) => resp.json())
-    .then(async function(data){
-        let htmlOutput = "";
-        for(let i=0; i<data.length; i++){
-            htmlOutput += "<p>"+ data[i] +"</p><hr>";
-        }
-        document.getElementById("searchResults")
-    })
+    if(text!=""){
+        fetch('../article/search/'+text, {
+            method: GET,
+            headers:{ 'Content-Type': 'application/json' }
+        })
+        .then((resp) => resp.json())
+        .then(async function(data){
+            let htmlOutput = "";
+            for(let i=0; i<data.length; i++){
+                htmlOutput += "<p>"+ data[i] +"</p><hr>";
+            }
+            document.getElementById("searchResults") = htmlOutput;
+        })
+    }
+    else{
+        fetch('../article/search/', {
+            method: GET,
+            headers:{ 'Content-Type': 'application/json' }
+        })
+        .then((resp) => resp.json())
+        .then(async function(data){
+            let htmlOutput = "";
+            for(let i=0; i<data.length; i++){
+                htmlOutput += "<p>"+ data[i] +"</p><hr>";
+            }
+            document.getElementById("searchResults") = htmlOutput;
+        })
+    }
 }
