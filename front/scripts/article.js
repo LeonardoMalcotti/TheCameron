bd63290d10af3934088948cd6be173175c736d28
elipanni <71708378+Elipanni-Francesca@users.noreply.github.com>
2021-04-29 12:49:57 +0200

    Fix correzioni

--- front/scripts/article.js
+++ front/scripts/article.js
@@ -3,14 +3,13 @@ function loadArticle(){
   var id = getUrlVars()["id"];
   var author = getUrlVars()["author"];
   var url = '../article/' + id + '/' + author;    //Costruisco l'url con l'id
-  console.log("id: "+id);
 
   fetch(url, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   })
-  .then((resp) => resp.json()) // Transform the data into json
-  .then(function(data) { // Here you get the data to modify as you please
+  .then((resp) => resp.json())
+  .then(function(data) {
     document.getElementById('txt_title').innerHTML = data.title;
     document.getElementById('txt_summary').innerHTML = data.summary;
     document.getElementById('txt_author').innerHTML = data.author;
