fa4d3b4304624c14f046d148eb24f9f30d891c71
LeonardoMalcotti <leonardo.malcotti@studenti.unitn.it>
2021-06-01 18:58:47 +0200

    fix test

--- back/httpCalls/test/tag.test.js
+++ back/httpCalls/test/tag.test.js
@@ -14,17 +14,18 @@ describe('tag test',()=> {
 		const Favorite = require("../../models/FavoriteTags");
 		const User = require("../../models/User");
 
-    Userspy = jest.spyOn(User,'findOne').mockImplementation((criterias) =>{
+    	Userspy = jest.spyOn(User,'findOne').mockImplementation((criterias) =>{
+			
 			if(criterias.username == "dantealighieri"){
-        return {
-  				name: "Dante",
-          surname: "Alighieri",
-          email: "dante.alighieri@loremipsum.it",
-          password: "12345678",
-          username: "dantealighieri"
-  			};
-      }
-      return {};
+		        return {
+		  				name: "Dante",
+		          		surname: "Alighieri",
+		          		email: "dante.alighieri@loremipsum.it",
+		          		password: "12345678",
+		          		username: "dantealighieri"
+		  			};
+      		}
+      		return null;
 		});
 
 		Tagspy = jest.spyOn(Tag,'find').mockImplementation((criterias) =>{
@@ -34,13 +35,13 @@ describe('tag test',()=> {
 				name : "Science"
 			},
 			{
-        id : 2,
-        name : "Politics"
-      },
+        		id : 2,
+        		name : "Politics"
+      		},
 			{
-        id : 3,
-        name : "Humor"
-      }];
+        		id : 3,
+        		name : "Humor"
+      		}];
 			return ret;
 		});
 
@@ -55,10 +56,10 @@ describe('tag test',()=> {
 			if(criterias.name == "Science"){
 				return ret;
 			}
-			return {};
+			return null;
 		});
 
-    FavoriteSpy = jest.spyOn(Favorite,'findOne').mockImplementation((criterias) =>{
+    	FavoriteSpy = jest.spyOn(Favorite,'findOne').mockImplementation((criterias) =>{
 			let ret = {
 				username : "dantealighieri",
 				id : [1,2]
@@ -66,16 +67,16 @@ describe('tag test',()=> {
 			if(criterias.username == "dantealighieri"){
 				return ret;
 			}
-			return {};
+			return null;
 		});
 	});
 
 	afterAll( async () =>{
-	   UserSpy.mockRestore();
-     Tagspy.mockRestore();
-     FavoriteSpy.mockRestore();
-		 TagSpyOne.mockRestore();
-   });
+	   	UserSpy.mockRestore();
+     	Tagspy.mockRestore();
+     	FavoriteSpy.mockRestore();
+		TagSpyOne.mockRestore();
+   	});
 
 //-------------------------------------------------------------------
 //tests
@@ -96,61 +97,68 @@ describe('tag test',()=> {
 	});*/
 
 	test("POST tag/:name, already exist", async done =>{
-		const response = await request.post("/tag/Science")
-    .set('Accept', 'application/json')
+		const response = await request.post("/tag/Science").set('Accept', 'application/json');
 
 		expect(response.statusCode).toBe(404);
+		done();
 	});
+
 
 	test("POST tag/:name, success", async done =>{
-		const response = await request.post("/tag/Romantic")
-    .set('Accept', 'application/json')
+		const response = await request.post("/tag/Romantic").set('Accept', 'application/json');
 
 		expect(response.statusCode).toBe(201);
+		done();
 	});
 
-	test("GET tag/user/:username", async done =>{
-    const response = await request.get("/tag/user/dantealighieri")
-    .set('Accept', 'application/json');
 
-		expect(response.body.id).toBe([1,2,3]);
-    expect(response.statusCode).toBe(201);
-  });
+	test("GET tag/user/:username", async done =>{
+    	const response = await request.get("/tag/user/dantealighieri").set('Accept', 'application/json');
+
+		expect(response.body.length).toBe(2);
+    	expect(response.statusCode).toBe(201);
+    	done();
+    });
+
 
 	test("GET tag/user/:username, miss user", async done =>{
-    const response = await request.get("/tag/user/username")
-    .set('Accept', 'application/json');
+    	const response = await request.get("/tag/user/username").set('Accept', 'application/json');
 
-    expect(response.statusCode).toBe(404);
-  });
+    	expect(response.statusCode).toBe(404);
+    	done();
+    });
+
 
 	test("GET tag/:name, success", async done =>{
-    const response = await request.get("/tag/user/Science")
-    .set('Accept', 'application/json');
+    	const response = await request.get("/tag/Science").set('Accept', 'application/json');
 
 		expect(response.body.id).toBe(1);
-    expect(response.statusCode).toBe(201);
-  });
+    	expect(response.statusCode).toBe(201);
+    	done();
+    });
+
 
 	test("GET tag/:name, miss tag", async done =>{
-    const response = await request.get("/tag/test")
-    .set('Accept', 'application/json');
+    	const response = await request.get("/tag/test").set('Accept', 'application/json');
 
-    expect(response.statusCode).toBe(404);
-  });
+    	expect(response.statusCode).toBe(404);
+    	done();
+    });
+
 
 	test("GET tag/id/:id, success", async done =>{
-    const response = await request.get("/tag/id/1")
-    .set('Accept', 'application/json');
+    	const response = await request.get("/tag/id/1").set('Accept', 'application/json');
 
 		expect(response.body.name).toBe("Science");
-    expect(response.statusCode).toBe(201);
-  });
+    	expect(response.statusCode).toBe(201);
+    	done();
+    });
+
 
 	test("GET tag/id/:id, miss tag", async done =>{
-    const response = await request.get("/tag/id/100")
-    .set('Accept', 'application/json');
+    	const response = await request.get("/tag/id/100").set('Accept', 'application/json');
 
-    expect(response.statusCode).toBe(404);
-  });
+    	expect(response.statusCode).toBe(404);
+    	done();
+  	});
 });
