a381026ef5632979821a4cd3d66d0d2e691997e9
LeonardoMalcotti <leonardo.malcotti@studenti.unitn.it>
2021-06-03 02:12:31 +0200

    cleaned

--- back/models/SavedArticles.js
+++ back/models/SavedArticles.js
@@ -4,6 +4,4 @@ var Schema = mongoose.Schema;
 module.exports = mongoose.model("savedArticles", new Schema({
 	username : {type: String, required: true},
 	articles : {type: [ {id:Number, author:String} ], required: true},
-	//id : {type: [Number], required: true},
-	//author: {type: [String], required: true},
 }));
