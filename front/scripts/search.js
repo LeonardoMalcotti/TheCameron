ec786edb2cbc3bec661f61d77505f36d1da5cf07
Filippo Fanton <filippo.fanton@studenti.unitn.it>
2021-06-03 13:01:03 +0200

    Fix ricerca tramite tag e autore

--- front/scripts/search.js
+++ front/scripts/search.js
@@ -1,6 +1,6 @@
 
 
-async function applyFilter() {
+function applyFilter() {
 	var author = document.getElementById("txt_author").value;
 	var strTags = document.getElementById("txt_tags").value;
 	strTags.replace(/%20/g, '')
@@ -20,25 +20,39 @@ async function applyFilter() {
 		url += ('author='+author);
 	}
 	if(tags && tags.length>0 && tags[0]!=""){
-		
-    
-	}
-advancedSearch(url);
-	
+		for(let i=0; i<tags.length; i++){
+			fetch("/tag/",{
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then((resp) => resp.json())
+      .then(function (data) {
+        for(x in data){
+          if(tags.includes(data[x].name)){
+            url += ('&tags[]='+data[x].id);
+          } 
+        }
+        advancedSearch(url);
+        return;
+      })      
+		}
+	}else{
+    advancedSearch(url);
+    return;
+  }
 }
 
-async function advancedSearch(url){
+function advancedSearch(url){
   fetch(url, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   })
   .then((resp) => resp.json())
   .then(async function(data){
+    articoli = [];
     for(i=0; i<data.length; i++){
       articoli[i] = data[i];
     }
-    //Cancelliamo la precedente ricerca
-    document.getElementById("article_list").innerHTML = "";
     printArticles(0);
   })
   .catch(error => console.log("errore: "+error));
