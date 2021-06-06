53d5ef08471356a18dbf2da5a116ab3391943496
Leonardo Malcotti <Leo@MacBook-Pro-di-Leonardo.local>
2021-06-06 23:02:48 +0200

    cleaned and commented

--- back/httpCalls/user.js
+++ back/httpCalls/user.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-//modello mongoose
 const User = require('../models/User');
 const Subscription = require('../models/Subscription');
 
 
 router.post("/",async (req,res)=>{
+	
+	//controlla che tutti i dati siano specificati
+
 	if (!req.body.name){
 		res.status(400).json({ error: "Nome dell' utente non specificato" });
 		return;
@@ -31,11 +33,10 @@ router.post("/",async (req,res)=>{
 		return;
 	}
 
-
 	//inserimento db
+
 	//controlla che non ci sia un altro utente con lo stesso username
-	let us = await User.find({'username':req.body.username});
-	if (us.length != 0){
+	if (await User.findOne({'username':req.body.username})){
 		res.status(403).json({ error: "Username già presente" });
 		return;
 	}
@@ -54,21 +55,14 @@ router.post("/",async (req,res)=>{
 });
 
 
-// user/:username GET
 router.get("/:username",async (req,res)=>{
 
-  let user = await User.findOne({'username':req.params.username});
+  	let user = await User.findOne({'username':req.params.username});
 
 	if(!user){
 		res.status(404).send();
 		return;
 	}
-
-	/*
-	if(user.length == 0){
-		res.status(404).json({errore: "Nessun user trovato"});
-		return;
-	}*/
 
 	res.status(200).json({
 		name : user.name,
@@ -80,7 +74,11 @@ router.get("/:username",async (req,res)=>{
 
 });
 
-// user/:username/subscription POST
+
+//Subscription --------------------------------------------------------------------
+
+//Crea una subscription per un dato utente
+//
 router.post("/:username/subscription",async(req,res)=>{
 
 	let user = await User.findOne({'username':req.params.username});
@@ -99,19 +97,21 @@ router.post("/:username/subscription",async(req,res)=>{
   	if(subscription){
 		res.status(404).json({errore:"Sei gia abbonato"});
 		return;
- 	 }
+ 	}
 
 	let sub = new Subscription({
 		username : req.params.username,
-    dateSubscription : req.body.dateSubscription
+   		dateSubscription : req.body.dateSubscription
 	});
 
 	sub.save();
 
 	res.location("/users/" + user.username + "/subscription").status(201).send();
-})
+});
 
-// user/:username/subscription GET
+
+//Recupera la subscripton di un dato utente
+//
 router.get("/:username/subscription",async (req,res)=>{
 
   	let subscription = await Subscription.findOne({'username':req.params.username});
@@ -127,10 +127,11 @@ router.get("/:username/subscription",async (req,res)=>{
 	}
 
 	res.status(200).json({username : subscription.username, dateSubscription : subscription.dateSubscription});
+});
 
-})
 
-// user/:username/subscription DELETE
+//Elimina la subscription di un utente
+//
 router.delete("/:username/subscription",async (req,res)=>{
 
   	let subscription = await Subscription.findOne({'username':req.params.username});
@@ -142,12 +143,12 @@ router.delete("/:username/subscription",async (req,res)=>{
 
 	let ret = await Subscription.deleteOne({'username':req.params.username});
 
-	if (ret) {
-		res.status(204).send();
-	} else {
+	if (!ret) {
 		res.status(400).send();
 		res.json({error: "Errore nella cancellazione dell'abbonamento"});
-	}
+	} 
+
+	res.status(204).send();
 });
 
 module.exports = router;
